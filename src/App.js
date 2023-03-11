e99b0d28567b7b7f91c94b699a22387d745f2b92
geodevcode <geodevinstitute@gmail.com>
2023-03-11 12:12:43 +0100

    NotFound.jsx was imported

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, useLocation } from "react-router-dom";
 import "./App.css";
 import Home from "pages/Home";
 import Contact from "pages/Contact";
@@ -7,21 +7,32 @@ import Services from "pages/Services";
 import Company from "pages/Company";
 import Careers from "pages/Careers";
 import Layout from "components/layoutComponent/Layout";
+import { AnimatePresence } from "framer-motion";
+import NotFound from "pages/NotFound";
+
+
+
+
+
 
 function App() {
+  const location = useLocation();
   return (
-    <div classnameName="App">
+    // <div classnameName="App">
       <Layout>
-        <Routes>
+        <AnimatePresence existBeforeEnter>
+        <Routes location={location} key={location.key}>
           <Route path="/" element={<Home />} />
           <Route path="contact" element={<Contact />} />
           <Route path="expertise" element={<Expertise />} />
           <Route path="services" element={<Services />} />
           <Route path="company" element={<Company />} />
           <Route path="careers" element={<Careers />} />
+          <Route path='*' element={<NotFound/>}/>
         </Routes>
+        </AnimatePresence>
       </Layout>
-    </div>
+    // </div>
   );
 }
 
